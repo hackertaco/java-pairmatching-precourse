67f892916c356bb2a4ffde8ab7802c31cec2439a
hackertaco <imsa1224@korea.ac.kr>
2023-12-02 14:30:04 +0900

    feat: show feature-2

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -2,7 +2,7 @@ package pairmatching;
 
 public class Application {
     public static void main(String[] args) {
-        PairController pairController = new PairController(new PairService());
+        PairController pairController = new PairController(new PairService(), new OutputView(), new InputView());
         pairController.start();
     }
 }
